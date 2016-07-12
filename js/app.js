1965cc6504b2d2cee24af497840d3d448a9e0cc2
schmo-dog <aaron.schmohe@gmail.com>
2016-07-12 13:53:11 -0600

    updated entire app

--- js/app.js
+++ js/app.js
@@ -1,147 +1,144 @@
-$(function() {
-    function AppViewModel() {
+function AppViewModel() {
 
-        var self = this;
-        var map;
-        var marker;
+    var self = this;
+    var map;
+    var marker;
+    var items = [{ lat: 44.7631, lng: -85.6206, Name: 'Traverse City' },
+        { lat: 44.8977, lng: -85.9889, Name: 'Glen Arbor' },
+        { lat: 44.8111, lng: -86.0601, Name: 'Empire' },
+        { lat: 44.8840923, lng: -86.04773089999998, Name: 'Sleeping Bear Dunes' },
+        { lat: 44.5536, lng: -86.2145, Name: 'Watervale' },
+    ];
 
-        var items = [{ lat: 44.7631, lng: -85.6206, Name: 'Traverse City' },
-            { lat: 44.8977, lng: -85.9889, Name: 'Glen Arbor', },
-            { lat: 44.8111, lng: -86.0601, Name: 'Empire', },
-            { lat: 44.8840923, lng: -86.04773089999998, Name: 'Sleeping Bear Dunes', },
-            { lat: 44.5536, lng: -86.2145, Name: 'Watervale', },
-        ];
+    locations = ko.observableArray();
 
-        locations = ko.observableArray();
+    // Create the Google Map
+    var infowindow = new google.maps.InfoWindow({});
+    var traverseCity = { lat: 44.819940, lng: -85.813971 };
 
-        // Create the Google Map
-        var traverseCity = { lat: 44.819940, lng: -85.813971 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: traverseCity,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    });
 
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 9,
-            center: traverseCity,
-            mapTypeId: google.maps.MapTypeId.TERRAIN
+    // loop through items array then build markers
+    for (var i = 0; i < items.length; ++i) {
+
+        var name = items[i].Name;
+        var location = items[i];
+        var position = {
+            lat: location.lat,
+            lng: location.lng
+        };
+        var lat = location.lat;
+        var lng = location.lng;
+
+        // create the markers, add them to their respective object in array
+        items[i].mapMarker = new google.maps.Marker({
+            position: position,
+            map: map,
+            label: name,
+            title: name,
+            animation: google.maps.Animation.DROP
         });
 
-        // loop through array then build markers
-        for (var i = 0; i < items.length; ++i) {
-
-            var name = items[i].Name;
-            var location = items[i];
-            var position = {
-                lat: location.lat,
-                lng: location.lng
-            };
-            var lat = location.lat;
-            var lng = location.lng;
-
-            // create the markers, add them to their respective object in array
-            items[i].mapMarker = new google.maps.Marker({
-                position: position,
-                map: map,
-                label: name,
-                title: name,
-                animation: google.maps.Animation.DROP
-
-            });
-
-            useInfo(items[i].mapMarker, name, lat, lng);
-        }
-
-        // build info window for the title of each marker
-        function useInfo(marker, title, lat, lng) {
-            var infowindow = new google.maps.InfoWindow({
-                content: title
-            });
-
-            // create listener when clicking on specific marker and do stuff
-            marker.addListener('click', function() {
-                infowindow.open(marker.get('map'), marker);
-                marker.setAnimation(google.maps.Animation.BOUNCE);
-                setTimeout(function() { marker.setAnimation(null); }, 750);
-
-                // pass marker info to Foursquare function and run it
-                loadData(lat, lng, title);
-            });
-        }
-
-        // Filter List with input box
-        // note- cities is the ul list in the sidebar
-        cities = ko.computed(function() {
-
-            /* These logs show that the markers have been
-            created and assigned to array before the filter runs and shows
-            that the visible property on the mapMarker is updated accordingly
-            */
-            console.log(items);
-            console.log('*-filter has been run-*');
-
-            // what is currently in the imput field
-            var currentContent = locations();
-
-            /* This will filter the cities list and will change the visible property
-              on each marker object within the array.
-
-            NOTE - This is where I need the help. I need to call the map again and tell
-            it that the visiblity properties have now changed again.
-            */
-            return items.filter(function(i) {
-
-                var filteredArray = i.Name.toLowerCase().indexOf(currentContent) >= 0;
-
-                if (filteredArray === true) {
-                    i.mapMarker.visible = true;
-                } else {
-                    i.mapMarker.visible = false;
-                }
-                return filteredArray;
-            });
-        });
-
-        // Load Forquare API
-        function loadData(lat, lng, title) {
-
-            var latitude = lat;
-            var longitude = lng;
-            var locationName = title;
-            var $foursquare = $('#foursquare');
-            var $foursquareArticles = $('#foursquare-articles');
-            var $foursquareHeaderElem = $('#foursquare-header');
-
-            // clear out old data before new request
-            $foursquareArticles.text('');
-
-            $foursquareHeaderElem.text('Nearby venue suggestions for ' + locationName + ' provided by Foursquare');
-
-            var $squareURL = 'https://api.foursquare.com/v2/venues/explore?client_id=DTSUJ5OJDHAHM3JCPZFIZU2GPSXBVVEVHDPIHKYKEPKOD3FM&client_secret=APWZMZEBYMYKHN20UX1N3CDMXU430NRGZFO3Z5DVBLTIUGVL&v=20130815&ll=' + latitude + ',' + longitude + '&limit=5';
-
-            // make call to Foursquare
-            $.getJSON($squareURL, function(data) {
-
-                venues = data.response.groups[0].items;
-
-                for (var i = 0; i < venues.length; i++) {
-                    var location = venues[i];
-                    $foursquareArticles.append('<li class="article">' +
-                        '<h3>' + location.venue.name + '</h3>' +
-                        '<p>' + 'Category: ' + location.venue.categories[0].name + '</p>' +
-                        '<p>' + 'Rating: ' + location.venue.rating + '</p>' +
-                        '</li>');
-                }
-
-            }).error(function(e) {
-                $foursquareHeaderElem.text('Yelp information could not be Loaded at this time.');
-            });
-
-            return false;
-        }
-
+        useInfo(items[i].mapMarker, name, lat, lng);
     }
 
-    ko.applyBindings(new AppViewModel());
+    function useInfo(marker, title, lat, lng) {
+        // create listener when clicking on specific marker and do stuff
+        marker.addListener('click', function() {
+            infowindow.setContent(title);
+            infowindow.open(marker.get('map'), marker);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() { marker.setAnimation(null); }, 750);
+            // pass marker info to Foursquare API function
+            loadData(lat, lng, title);
+        });
+    }
 
-    // Functionality to toggle menu bar when in mobile view
-    $(".navbar-toggle").on('click', function() {
-        $('.sidebar').toggleClass('show');
+    // when a specific location in the list has been clicked
+    self.listItemClick = function(marker) {
+        var theMarker = marker.mapMarker;
+        new google.maps.event.trigger(theMarker, 'click');
+    };
+
+    // flter list with the input box
+    cities = ko.computed(function() {
+
+        // close any marker info windows
+        infowindow.close();
+
+        // what is currently in the imput field
+        var currentContent = locations();
+
+        return items.filter(function(i) {
+
+            var filteredArray = i.Name.toLowerCase().indexOf(currentContent) >= 0;
+            // Set the visiblity of the markers depending if they are in the filtered list
+            if (filteredArray === true) {
+                i.mapMarker.setVisible(true);
+            } else {
+                i.mapMarker.setVisible(false);
+            }
+            return filteredArray;
+        });
     });
+
+    // Load Forquare API
+    function loadData(lat, lng, title) {
+
+        var latitude = lat;
+        var longitude = lng;
+        var locationName = title;
+        var $foursquare = $('#foursquare');
+        var $foursquareArticles = $('#foursquare-articles');
+        var $foursquareHeaderElem = $('#foursquare-header');
+
+        $($foursquare).show();
+        $foursquareArticles.text('');
+
+        $foursquareHeaderElem.text('Nearby venue suggestions for ' + locationName + ' provided by Foursquare');
+
+        var $squareURL = 'https://api.foursquare.com/v2/venues/explore?client_id=DTSUJ5OJDHAHM3JCPZFIZU2GPSXBVVEVHDPIHKYKEPKOD3FM&client_secret=APWZMZEBYMYKHN20UX1N3CDMXU430NRGZFO3Z5DVBLTIUGVL&v=20130815&ll=' + latitude + ',' + longitude + '&limit=5';
+
+        // make call to Foursquare
+        $.getJSON($squareURL, function(data) {
+
+            venues = data.response.groups[0].items;
+
+            for (var i = 0; i < venues.length; i++) {
+                var location = venues[i];
+                $foursquareArticles.append('<li class="article shadow">' +
+                    '<h3>' + location.venue.name + '</h3>' +
+                    '<p>' + 'Category: ' + location.venue.categories[0].name + '</p>' +
+                    '<p>' + 'Rating: ' + location.venue.rating + '</p>' +
+                    '</li>');
+            }
+
+        }).error(function(e) {
+            $foursquareHeaderElem.text('Foursquare information could not be loaded at this time. Please try again later.');
+        });
+
+        return false;
+    }
+}
+
+ko.applyBindings(new AppViewModel());
+
+// hide the Foursquare div before the API function has been called
+$('#foursquare').hide();
+
+// menu funtionality in mobile view
+var $sidebar = $('.sidebar');
+
+$(".navbar-toggle").on('click', function() {
+    $sidebar.toggleClass('show');
+});
+
+$(".cities").children().on('click', function() {
+    if ($sidebar.hasClass('show')) {
+        $sidebar.toggleClass('show');
+    }
 });
